0d80e393439ad78ab391b4f5911ebc1cda58fd2a
Taka Okunishi <okunishinishi@gmail.com>
2014-10-05 22:28:24 +0900

    modify title binding

--- public/javascripts/ok/services.js
+++ public/javascripts/ok/services.js
@@ -111,18 +111,16 @@
 
     ng
         .module('ok.services')
-        .service('langDetectService', function LangDetectService(localeConstant,
-                                                                 $location,
-                                                                 multiLangUrlLogic) {
-            var s = this;
+        .service('langDetectService', function LangDetectService(metaConstant, $location, multiLangUrlLogic) {
+            var s = this,
+                SUPPORTED_LANGS = metaConstant.SUPPORTED_LANGS,
+                DEFAULT_LANG = SUPPORTED_LANGS[0];
 
-            s.supportedLangs = Object.keys(localeConstant);
-
-            var defaultLang = 'en';//FIXME
-
+            s.SUPPORTED_LANGS = SUPPORTED_LANGS;
+            s.DEFAULT_LANG = DEFAULT_LANG;
             s.detectLang = function () {
                 var url = $location.href;
-                return multiLangUrlLogic.langForUrl(url, s.supportedLangs) || defaultLang;
+                return multiLangUrlLogic.langForUrl(url, SUPPORTED_LANGS) || DEFAULT_LANG;
             };
 
         });
