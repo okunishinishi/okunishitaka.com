432765f2eb33a019de109cce4488f5cde55d3728
Taka Okunishi <okunishinishi@gmail.com>
2014-11-05 07:12:09 +0900

    refactroing [ci skip]

--- public/javascripts/ok/constants/url/page-url-constant.js
+++ public/javascripts/ok/constants/url/page-url-constant.js
@@ -9,7 +9,6 @@
         .module('ok.constants')
         .constant('pageUrlConstant', {
 		    "ADMIN": "/admin.html",
-		    "BLOG_EDIT": "/blog-edit.html",
 		    "BLOG": "/blog.html",
 		    "INDEX": "/index.html",
 		    "PROFILE": "/profile.html",
